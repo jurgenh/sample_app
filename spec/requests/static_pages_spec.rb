f3876b2ef03f9a842f680ac3d66045db3053d0ff
Jurgen Herrmann <jherrmann@oreilly.com>
2013-10-29 16:37:46 -0700

    add rspec support utility for availability in tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,28 +23,28 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_title('Help')}
+    it { should have_title(full_title('Help'))}
     it { should have_content('Help')}
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_title('About')}
+    it { should have_title(full_title('About'))}
     it { should have_content('About Us')}
   end
 
     describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_title('Contact')}
+    it { should have_title(full_title('Contact'))}
     it { should have_content('Contact')}
   end
 end
