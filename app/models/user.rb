ca877713585381dcc7d4c97571e699913c5a55fd
Jurgen Herrmann <jherrmann@oreilly.com>
2013-11-22 11:07:14 -0800

    fix double dot problem regex for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 
   validates :name,  presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # allowed double dots VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness:  { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 
 
